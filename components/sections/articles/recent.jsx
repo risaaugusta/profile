ef86431c3b97318e7338349e1ede51db6fa0b8d3
risaaugusta <augustariris20@gmail.com>
2024-06-01 23:55:46 +0700

    update

--- components/sections/articles/recent.jsx
+++ components/sections/articles/recent.jsx
@@ -1,64 +1,212 @@
-import Section 		from '../../structure/section';
-import Container 	from '../../structure/container';
+import Section from "../../structure/section";
+import Container from "../../structure/container";
 
-import Image from 'next/image'
-import SectionTitle from '../../blocks/section.title.block'
+import Image from "next/image";
+import SectionTitle from "../../blocks/section.title.block";
 
-import Icon from '../../utils/icon.util'
+import Icon from "../../utils/icon.util";
 
-import css from '../../../styles/sections/articles/recent.module.scss'
+import css from "../../../styles/sections/articles/recent.module.scss";
 
 export default function Recent({ mediumArticles }) {
+  const feed = mediumArticles.feed;
+  const articles = mediumArticles.items;
 
-	const feed 		= mediumArticles.feed
-	const articles 	= mediumArticles.items
+  return (
+    <Section classProp="borderBottom">
+      <Container spacing={"verticalXXXXLrg"}>
+        <SectionTitle
+          title="Articles"
+          preTitle="Personal"
+          subTitle="A personal quest to become a better creative writer."
+        />
 
-	return (
-		<Section classProp="borderBottom">
-			<Container spacing={'verticalXXXXLrg'}>
-				{/* <SectionTitle
-					title="Recent Articles"
-					preTitle="Informative"
-					subTitle="A personal quest to become a better creative writer."
+        {/* <SectionTitle
+					title="Coming soon" 
 				/> */}
 
-				<SectionTitle
-					title="Coming soon" 
-				/>
-
-				{/* <section className={css.projects}>
-					{
-					articles.map( ({ title, pubDate, link, author, thumbnail, categories }, index) => {
-						const date = new Date(pubDate).toDateString()
-						return (
-							<>
-							<article key={index} className={css.project}>
-								<span className={css.featuredImage}>
+        {/* <section className={css.projects}>
+          {articles.map(
+            (
+              { title, pubDate, link, author, thumbnail, categories },
+              index
+            ) => {
+              const date = new Date(pubDate).toDateString();
+              return (
+                <>
+                  <article key={index} className={css.project}>
+                    <span className={css.featuredImage}>
 									<img src={thumbnail} alt="Article thumbnail" />
 								</span>
-								<span className={css.header}>
-									<a href={link} rel="noreferrer" target="_blank">{title} <Icon icon={[ 'fad', 'arrow-up-right-from-square' ]} /></a>
-								</span>
-								<span className={css.descriptionContainer}>
-								</span>
-								<span className={css.details}>
-									<p>By {author}</p>
-									<p className={css.pushedAt}>{date}</p>
-								</span>
-								<span className={css.topicsContainer}>
-									{
-									categories.map( (e, index) => {
-										return ( <span key={index} className={css.topics}><Icon icon={[ 'fab', 'medium' ]} /> {e}</span> )
-									})
-									}
-								</span>
-							</article>
-							</>
-						)
-					})
-					}
-				</section> */}
-			</Container>
-		</Section>
-	)
+                    <span className={css.header}>
+                      <a href={link} rel="noreferrer" target="_blank">
+                        {title}{" "}
+                        <Icon icon={["fad", "arrow-up-right-from-square"]} />
+                      </a>
+                    </span>
+                    <span className={css.descriptionContainer}></span>
+                    <span className={css.details}>
+                      <p>By {author}</p>
+                      <p className={css.pushedAt}>{date}</p>
+                    </span>
+                    <span className={css.topicsContainer}>
+                      {categories.map((e, index) => {
+                        return (
+                          <span key={index} className={css.topics}>
+                            <Icon icon={["fab", "medium"]} /> {e}
+                          </span>
+                        );
+                      })}
+                    </span>
+                  </article>
+                </>
+              );
+            }
+          )}
+        </section> */}
+
+        <section className={css.projects}>
+          {/* ARTICLE 2 */}
+          <article className={css.project}>
+            <span className={css.header}>
+              <a
+                href={
+                  "https://www.linkedin.com/feed/update/urn:li:activity:7197213477113905152/"
+                }
+                rel="noreferrer"
+                target="_blank"
+              >
+                Congraduation! Bachelor of Computer Science{" "}
+                <Icon icon={["fad", "arrow-up-right-from-square"]} />
+              </a>
+            </span>
+            <span className={css.descriptionContainer}></span>
+            <span className={css.details}>
+              <p>By Risa Augusta</p>
+              <p className={css.pushedAt}>May 2024</p>
+            </span>
+            <span className={css.topicsContainer}>
+              <span className={css.topics}>
+                <Icon icon={["fas", "pen-to-square"]} /> Education
+              </span>
+              <span className={css.topics}>
+                <Icon icon={["fas", "graduation-cap"]} /> Graduation
+              </span>
+            </span>
+          </article>
+
+          {/* ARTICLE 3 */}
+          <article className={css.project}>
+            <span className={css.header}>
+              <a
+                href={
+                  "https://www.linkedin.com/feed/update/urn:li:activity:7197213477113905152/"
+                }
+                rel="noreferrer"
+                target="_blank"
+              >
+                Internship at Telkom Indonesia - MSIB Batch 4
+                <Icon icon={["fad", "arrow-up-right-from-square"]} />
+              </a>
+            </span>
+            <span className={css.descriptionContainer}></span>
+            <span className={css.details}>
+              <p>By Risa Augusta</p>
+              <p className={css.pushedAt}>June 2023</p>
+            </span>
+            <span className={css.topicsContainer}>
+              <span className={css.topics}>
+                <Icon icon={["fas", "briefcase"]} /> Career
+              </span>
+              <span className={css.topics}>
+                <Icon icon={["fas", "code"]} /> Developer
+              </span>
+            </span>
+          </article>
+
+          {/* ARTICLE 4 */}
+          <article className={css.project}>
+            <span className={css.header}>
+              <a
+                href={
+                  "https://www.linkedin.com/feed/update/urn:li:activity:6923117401299464192/"
+                }
+                rel="noreferrer"
+                target="_blank"
+              >
+                Pilmapres
+                <Icon icon={["fad", "arrow-up-right-from-square"]} />
+              </a>
+            </span>
+            <span className={css.descriptionContainer}></span>
+            <span className={css.details}>
+              <p>By Risa Augusta</p>
+              <p className={css.pushedAt}>March 2022</p>
+            </span>
+            <span className={css.topicsContainer}>
+              <span className={css.topics}>
+                <Icon icon={["fas", "pen-to-square"]} /> Education
+              </span>
+            </span>
+          </article>
+
+          {/* ARTICLE 5 */}
+          <article className={css.project}>
+            <span className={css.header}>
+              <a
+                href={
+                  "https://www.linkedin.com/feed/update/urn:li:activity:6807657692719579137/"
+                }
+                rel="noreferrer"
+                target="_blank"
+              >
+                Beasiswa Unggulan Annual Gathering
+                <Icon icon={["fad", "arrow-up-right-from-square"]} />
+              </a>
+            </span>
+            <span className={css.descriptionContainer}></span>
+            <span className={css.details}>
+              <p>By Risa Augusta</p>
+              <p className={css.pushedAt}>June 2021</p>
+            </span>
+            <span className={css.topicsContainer}>
+              <span className={css.topics}>
+                <Icon icon={["fas", "pen-to-square"]} /> Education
+              </span>
+              <span className={css.topics}>
+                <Icon icon={["fas", "money-bill-wave"]} /> Scholarship
+              </span>
+            </span>
+          </article>
+          <article className={css.project}>
+            <span className={css.header}>
+              <a
+                href={
+                  "https://www.linkedin.com/feed/update/urn:li:activity:6877445805855375360/"
+                }
+                rel="noreferrer"
+                target="_blank"
+              >
+                Youth Exchange Program - Chapter Turkey
+                <Icon icon={["fad", "arrow-up-right-from-square"]} />
+              </a>
+            </span>
+            <span className={css.descriptionContainer}></span>
+            <span className={css.details}>
+              <p>By Risa Augusta</p>
+              <p className={css.pushedAt}>Dec 2021</p>
+            </span>
+            <span className={css.topicsContainer}>
+              <span className={css.topics}>
+                <Icon icon={["fas", "pen-to-square"]} /> Education
+              </span>
+              <span className={css.topics}>
+                <Icon icon={["fas", "plane"]} /> Study abroad
+              </span>
+            </span>
+          </article>
+        </section>
+      </Container>
+    </Section>
+  );
 }
