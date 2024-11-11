49c4b308f902335b59b3326d16c8234310afbbf0
risaaugusta <augustariris20@gmail.com>
2024-11-11 15:02:44 +0700

    update career period

--- components/sections/index/career.jsx
+++ components/sections/index/career.jsx
@@ -35,7 +35,7 @@ export default function Career() {
               <span className={career.companyHeader}>
                 <h3>PT Bringin Inti Teknologi </h3>
                 <h4>Front End Mobile Developer | Full-time</h4>
-                <h4>Jan 2024 - Present · 9 mos</h4>
+                <h4>Jan 2024 - Present · 11 mos</h4>
                 <h5>Jakarta</h5>
               </span>
               <ul className={career.list}>
